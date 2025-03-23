b66b568552832d0b4c002379d8425c912913b983
Caleb Simmons <csimmons718@bears.shawu.edu>
2025-03-22 23:23:48 -0400

    dashboard header nav

--- client/src/dashboard/dashboard.jsx
+++ client/src/dashboard/dashboard.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import "./dashboard-styles.css";
 
 export const Dashboard = () => {
-  return <div>dashboard</div>;
+  return (
+    <>
+      <div className="dashboard-grid">
+        <div className="header">
+          <div className="header-item">
+            <button>TeamManagerHub</button>
+          </div>
+          <div className="header-item">
+            <button>Teams</button>
+            <button>Order</button>
+          </div>
+          <div className="header-item">
+            <button>Profile</button>
+          </div>
+        </div>
+        <div className="sidebar"></div>
+        <div className="main-content"></div>
+      </div>
+    </>
+  );
 };
