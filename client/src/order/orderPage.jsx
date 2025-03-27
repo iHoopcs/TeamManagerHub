9542155bc8c7ab4fb6e802fd514ce1a7c63553b0
Caleb Simmons <csimmons718@bears.shawu.edu>
2025-03-27 03:04:37 -0400

    orderPage & styles

--- client/src/order/orderPage.jsx
+++ client/src/order/orderPage.jsx
@@ -1,21 +1,30 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import "./orderPage-styles.css";
+
 export const OrderPage = (props) => {
   const { name } = props;
 
   const nav = useNavigate();
   const changeRestaurant = () => {
     //redirect to dashboard
-    //open order modal - display restaurants
+    //re render order modal on /dashboard - display restaurants
     nav("/dashboard", {
       replace: true,
       state: { openOrderModal: true },
     });
   };
   return (
-    <>
-      <h1>Welcome to {name}!</h1>
+    <div className="order-container">
       <button onClick={changeRestaurant}>Change restaurant</button>
-    </>
+      <div className="order-content">
+        <h1>Welcome to {name}!</h1>
+        {/* TODO: */}
+        {/* display team info & members one by one? */}
+        {/* display menu - breakfast, lunch, dinner w/ price */}
+        {/* text message to each player */}
+        {/* receive response - fill in order for each member */}
+      </div>
+    </div>
   );
 };
