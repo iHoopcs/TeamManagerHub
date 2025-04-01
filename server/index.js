a160c3153e7d7b81bdf95198a07428c97f325ec9
Caleb Simmons <csimmons718@bears.shawu.edu>
2025-04-01 01:40:48 -0400

    move app --> index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,41 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-const app = require("./app");
+const express = require("express");
+const cors = require("cors");
+const passport = require("passport");
+const authRoutes = require("./routes/authRoutes");
+const teamRoutes = require("./routes/teamRoutes");
+require("./config/strategy-config");
+const session = require("express-session");
+
+const app = express();
+
+//middleware
+app.use(cors({ origin: "*" }));
+app.use(express.json());
+
+app.use(
+  session({
+    saveUninitialized: false,
+    secret: "Christ is Lord",
+    save: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//routes
+app.use("/api/auth", authRoutes);
+app.use("/api/", teamRoutes);
+//login authentication route
+app.post(
+  "/api/passport-auth",
+  passport.authenticate("local"),
+  (req, res, next) => {
+    res.status(200).send("Logged in successfully!");
+  }
+);
 
 //db
 mongoose
